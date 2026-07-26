7b05fb9d2b3ecbe15ead8790b2e39cdb70483347
Tanzeel <tanzeel123raza@gmail.com>
2026-07-26 17:21:51 +0530

    changes

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-# import math
+import math
 
 def addition(x, y):
     return x+y
@@ -11,19 +11,57 @@ def multiplication(x, y):
 
 def division(x, y):
     if(y == 0):
-        return "Cannot divide  by zero"
+        return "Cannot divide  by zero '0'. "
     return x/y
+
+def percentage(x, y):
+    if(y == 0):
+        return " Cannot divide by zero '0'. "
+    return (x/y)*100
+
+def power(x, y):
+    return pow(x, y)
+
+def modulus(x, y):
+    return x % y
+
+def floorDivision(x, y):
+    if(y == 0):
+        return "Cannot divide by zero '0'. "
+    return x//y
+
+def squareRoot(z):
+    if(z < 0):
+        return "Square root of -ve integer isn't possible"
+    return math.sqrt(z)
 
 def getNumber():
     x = float(input("Enter your 1st number: "))
     y = float(input("Enter your 2nd number: "))
     return x, y
 
+History = []
+
 while True:
+
+    print("\n===== Calculator =====")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Percentage")
+    print("6. Power")
+    print("7. Modulus")
+    print("8. Floor Division")
+    print("9. Square Root")
+    print("10. Show History")
+    print("11. Clear History")
+    print("12. Exit")
+        
     try:
         choice = int(input("Enter your choice: "))
 
-        if choice in (1, 2, 3, 4):
+        if choice in (1, 2, 3, 4, 5, 6, 7, 8):
             x, y = getNumber()
 
     except ValueError:
@@ -31,18 +69,64 @@ while True:
         continue
 
     if choice == 1:
-        print("Answer= ", addition(x, y))
+        answer = addition(x, y)
+        print("Answer= ", answer)
+        History.append(f"{x} + {y} = {answer}")
 
     elif choice == 2:
-        print("Answer= ", subtraction(x, y))
+        answer = subtraction(x, y)
+        print("Answer= ", answer)
+        History.append(f"{x} - {y} = {answer}")
 
     elif choice == 3:
-        print("Answer= ", multiplication(x, y))
+        answer = multiplication(x, y)
+        print("Answer= ", answer)
+        History.append(f"{x}*{y} = {answer}")
 
     elif choice == 4:
-        print("Answer= ", division(x, y))
+        answer= division(x, y)
+        print("Answer= ", answer)
+        History.append(f"{x} / {y} = {answer}")
 
     elif choice == 5:
+        answer = percentage(x, y)
+        print("Answer= ", answer)
+        History.append(f"({x} / {y}) * {100} = {answer}")
+
+    elif choice == 6:
+        answer = power(x, y)
+        print("Answer= ", answer)
+        History.append(f"{x} ^ {y} = {answer}")
+
+    elif choice == 7:
+        answer = modulus(x, y)
+        print("Answer= ", answer)
+        History.append(f"{x} % {y} = {answer}")
+
+    elif choice == 8:
+        answer = floorDivision(x, y)
+        print("Answer= ", answer)
+        History.append(f"{x} // {y} = {answer}")
+
+    elif choice == 9:
+        z = int(input("Enter your number: "))
+        answer = squareRoot(z)
+        print("Answer= ", answer)
+        History.append(f"sprt({z}) = {answer}")
+
+    elif choice == 10:
+        if not History:
+            print("No history available.")
+        else:
+            print("\n---- History ----")
+            for item in History:
+                print(item)
+
+    elif choice == 11:
+        History.clear()
+        print("-- History cleared. --")
+
+    elif choice == 12:
         print("Calculation ended. ")
         break
 
